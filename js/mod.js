37259019e292eecbd9d7c119c9c7254c3e233497
Brandly123 <FlightlessBird0604@gmail.com>
2025-12-20 17:02:30 -0800

    v2”

--- js/mod.js
+++ js/mod.js
@@ -12,14 +12,12 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "1.0",
-	name: "Literally nothing",
+	num: "2",
+	name: "Rebalances!",
 }
 
-let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+let changelog = `<h1>hi</h1>
+`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
